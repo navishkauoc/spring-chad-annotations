2d31c4aace0e23bcc3c257abd63c8598ab61ccc3
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-17 20:39:07 +0530

    RandomFortuneService updated

--- src/com/navishka/springdemo/RandomFortuneService.java
+++ src/com/navishka/springdemo/RandomFortuneService.java
@@ -1,14 +1,30 @@
 package com.navishka.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	//Create an array of strings
+	private String[] data = {
+			"Beware of Dogs",
+			"Diligence is mother of good luck",
+			"Wisdom Enlightens"
+	};
+	
+	//Create a random number generator
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		//Pick a random string from array
+		int index = myRandom.nextInt(data.length);
+		
+		String theFortune = data[index];
+		
+		return theFortune;
 	}
 
 }
