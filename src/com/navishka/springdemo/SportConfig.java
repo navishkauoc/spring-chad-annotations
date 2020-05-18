bd72f6dd5f42682367f64fbf5bfdf9c67639eb8f
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-18 23:24:53 +0530

    Spring configuration with Java code

---
+++ src/com/navishka/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.navishka.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.navishka.springdemo")
+public class SportConfig {
+
+}
