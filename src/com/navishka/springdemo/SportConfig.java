1c38e708a1c9535838b5c94f98dc3f11b22704ca
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-19 20:25:36 +0530

    Defined method to expose bean

--- src/com/navishka/springdemo/SportConfig.java
+++ src/com/navishka/springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package com.navishka.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.navishka.springdemo")
 public class SportConfig {
 
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	@Bean
+	public Coach swimCoach() {
+		SwimCoach mySwimCoach = new SwimCoach(sadFortuneService());
+		return mySwimCoach;
+	}
+	
 }
