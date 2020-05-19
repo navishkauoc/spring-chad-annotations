d01461360de7bf5ab50ab3b8217fc743e8ebb1b0
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-19 20:08:57 +0530

    New fortune service - SadFortuneService

---
+++ src/com/navishka/springdemo/SadFortuneService.java
@@ -0,0 +1,10 @@
+package com.navishka.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return "Today is a sad day!";
+	}
+
+}
---
+++ src/com/navishka/springdemo/SwimCoach.java
@@ -0,0 +1,15 @@
+package com.navishka.springdemo;
+
+public class SwimCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return null;
+	}
+
+}
