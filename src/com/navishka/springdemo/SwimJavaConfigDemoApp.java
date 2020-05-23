a0c9edc92340289c3bef79288b522b67c4ef238c
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-23 13:39:00 +0530

    Injecting values from a properties file

--- src/com/navishka/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/navishka/springdemo/SwimJavaConfigDemoApp.java
@@ -7,12 +7,16 @@ public class SwimJavaConfigDemoApp {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
 		
+		System.out.println("Email: " + theCoach.getEmail());
+
+		System.out.println("Team: " + theCoach.getTeam());
+
 		context.close();
 
 	}
